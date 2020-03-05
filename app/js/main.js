f8486f8562fade93c5d8470588ed8b8a65356400
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2020-03-05 08:31:26 +0100

    Add functions to support sending messages

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
     mainStick();
     goTopButton();
     obslugaGalerii();
+    sendingMail();
+    formIsFilled();
 })
 
 function mainStick() {
@@ -45,8 +47,71 @@ function goTopButton() {
 
 function obslugaGalerii() {
     $("#gallery img").hover(function () {
-        $(this).siblings().css('opacity', 0.85);
+        $(this).siblings().css('opacity', 0.65);
     }, function () {
         $(this).siblings().css('opacity', 1);
     })
 }
+
+// Frmularz PHP - START
+
+function formIsFilled() {
+    console.log('im in')
+    $('form input, form textarea').focus(function () {
+        $(this).parent().addClass('focus');
+    })
+    $('form input, form textarea').focusout(function () {
+        if ($(this).val() !== "") {
+            $(this).addClass('filled');
+        } else {
+            $(this).parent().removeClass('focus');
+        }
+    })
+}
+
+
+
+
+function sendingMail(){
+    var infoDisplayer = $(".formMessage");
+    var form = $("#form1");
+    
+
+    form.on('submit', function (e){
+        $.ajax({
+                url: "php/mail.php",
+                dataType: "JSON",
+                type: "post",
+                data:$(this).serialize(),
+                beforeSend: function(){
+                    infoDisplayer.hide();
+                    infoDisplayer.removeClass("ok error");
+                    infoDisplayer.text('trwa wysyłanie danych...').slideDown(300);
+                    console.log("beforeSend");
+                },
+                success: function(obj){
+                    if (obj.type=="ok")
+                    {
+                        infoDisplayer.addClass("ok").removeClass("error").html(obj.text).delay(4000).slideUp(500);
+                        // form.get(0).reset();
+                        console.log("success - ok");
+                    } else
+                    {
+                       infoDisplayer.addClass("error").removeClass("ok").html(obj.text);
+                       console.log("success - error");
+                    }
+                },
+                error : function(){
+                    infoDisplayer.addClass("error").removeClass("ok").html("Wystąpił błąd podczas wysyłania informacji.");
+                    console.log("error");
+                },
+                complete: function(){
+                   // infoDisplayer.fadeIn();
+                   console.log("complete");
+                }
+           });
+        e.preventDefault();
+    })
+}
+
+// Frmularz PHP - END
